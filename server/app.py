0c4065cb2c03c5c8994668631630a79739a12dbb
AllenCodess <Aph296@protonmail.com>
2025-11-11 04:46:59 -0500

    flask application

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to Flatiron Cars</h1>'
+
+@app.route('/<string:carmodel>')
+def model(carmodel):
+    if carmodel in existing_models:
+        return f"Flatiron {carmodel} is in our fleet!"
+    if carmodel not in existing_models:
+        return f"No models called {carmodel} exist in our catalog"
